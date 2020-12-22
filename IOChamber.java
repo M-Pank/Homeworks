145f73ffccaff756c066145938d8fbe54f92cb1b
M-Pank <68184758+M-Pank@users.noreply.github.com>
2020-12-22 13:41:49 +0300

    Update IOChamber.java

--- IOChamber.java
+++ IOChamber.java
@@ -1,39 +1,21 @@
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.nio.file.Files;
 import java.util.List;
 import java.io.IOException;
+import java.lang.ArrayIndexOutOfBoundsException;
 
-
-public class IOChamber {
-    public static void main(String[] patch) {IOChamber();}
-    public static void Echo() {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList echoes = new ArrayList();
-        String line = "";
-        while ((line = scanner.nextLine()).length() > 0) {
-            echoes.add(line);
-        }
-        for (Object echo : echoes) {
-            if (echoes.indexOf(echo) %2 == 0) {
-                System.out.println(echo);
-            }
-
-        }
-    }
-    public static void IOChamber(){
-        Scanner scanner = new Scanner(System.in);
+public class IOChamber extends EchoChamber {
+    public static void main(String[] args) {
         try {
-            List<String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
+            List<String> lines = Files.readAllLines(Paths.get(args[0]));
             for (String s: lines) {
                 if (lines.indexOf(s) % 2 == 0) {
                     System.out.println(s);
                 }
             }
-        } catch (IOException e) {
+        } catch (ArrayIndexOutOfBoundsException | IOException e) {
             System.out.println("Файл не распознан. Введите ваши сообщения");
-            Echo();
+            echo();
         }
     }
 }
