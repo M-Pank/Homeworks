904c90eaca60110c0a9aa69e65241b75ac5e9f01
M-Pank <68184758+M-Pank@users.noreply.github.com>
2020-12-22 14:46:24 +0300

    Update IOChamber.java

--- IOChamber.java
+++ IOChamber.java
@@ -8,10 +8,8 @@ public class IOChamber extends EchoChamber {
     public static void main(String[] args) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(args[0]));
-            for (String s: lines) {
-                if (lines.indexOf(s) % 2 == 0) {
-                    System.out.println(s);
-                }
+            for (int i = 0; i < lines.size(); i+=2) {
+                    System.out.println(lines.get(i));
             }
         } catch (ArrayIndexOutOfBoundsException | IOException e) {
             System.out.println("Файл не распознан. Введите ваши сообщения");
